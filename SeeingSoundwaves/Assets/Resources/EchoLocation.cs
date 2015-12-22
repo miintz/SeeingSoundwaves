5af79d75898225ebb1163da70b3e8275856c7c69
miintz <vanheesmaarten@hotmail.com>
2015-12-22 17:29:33 +0100

    maarten: included camera position, but it broke the collisions. needs fix

--- SeeingSoundwaves/Assets/Resources/EchoLocation.cs
+++ SeeingSoundwaves/Assets/Resources/EchoLocation.cs
@@ -5,40 +5,43 @@ using System.IO;
 using System.Collections.Generic;
 using UnityStandardAssets.Characters.FirstPerson;
 
-public class EchoLocation : MonoBehaviour {
+public class EchoLocation : MonoBehaviour
+{
 
     public bool EnableEchoLocation = true;
 
     private Quaternion CameraRotation;
     private Quaternion CharacterRotation;
-    private Vector3 PositionOfPlayer;
+    private Vector3 CameraPosition;
 
     private FirstPersonControllerMod Character;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         if (EnableEchoLocation)
         {
             Character = GetComponent("FirstPersonControllerMod") as FirstPersonControllerMod;
             Character.m_Block = true; //singleton!!! handig
         }
-	}
-   
-	// Update is called once per frame
-	void Update () {        
+    }
+
+    // Update is called once per frame
+    void Update() {        
         if (EnableEchoLocation)
         {
-            //PositionOfPlayer = Character.GetCameraPosition();
+            CameraPosition = Character.GetCameraMoveDirection();
             CameraRotation = Character.GetCameraRotation();
             CharacterRotation = Character.GetCharacterRotation();
 
             //CharacterRotation is de FPSController gameobject (over X)
             //CameraRotation is de camera IN de FPSController (over Y)
             
-            if (Input.GetKeyDown(KeyCode.Space))
+            if (Input.GetKeyDown(KeyCode.F))
             {
                 transform.localRotation = CharacterRotation;
-                Camera.main.transform.localRotation = CameraRotation;            
+                Camera.main.transform.localRotation = CameraRotation;
+                transform.localPosition = CameraPosition;
             }
         }
 	}
